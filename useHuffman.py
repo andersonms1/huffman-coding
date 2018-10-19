8de71517775d0db7a5143f8942014a7c3b118db3
Anderson Silva <andersoonn.ams@gmail.com>
2018-10-19 00:00:55 -0300

    Add percent
    Co-authored-by: Anderson Silva <andersoonn.ams@gmail.com>
    Co-authored-by: Ulysses Lara <ulysses3353@gmail.com>

--- useHuffman.py
+++ useHuffman.py
@@ -1,7 +1,13 @@
 from huffman import HuffmanCoding
 import sys
+import os
 
-path = "sample.txt"
+if len(sys.argv) == 1:
+
+    path = "sample.txt"
+
+else:
+    path = sys.argv[1]
 
 h = HuffmanCoding(path)
 
@@ -10,3 +16,9 @@ print("Compressed file path: " + output_path)
 
 decom_path = h.decompress(output_path)
 print("Decompressed file path: " + decom_path)
+
+percent_reduce = os.path.getsize(output_path) * 100 / os.path.getsize(path)
+print('Original file -> ' + str(os.path.getsize(path)))
+print('Compressed file -> ' + str(os.path.getsize(output_path)))
+print(percent_reduce)
+
